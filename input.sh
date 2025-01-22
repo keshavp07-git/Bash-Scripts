bfa9e31e9dd5d0f1a7a6a4642e204defa69436ae
keshavp07-git <keshav.prajapati830@gmail.com>
2025-01-22 17:55:11 +0530

    Input based bash

---
+++ input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+echo "WELCOME TO LOGIN"
+echo "-----------------------------------------------------------------------------"
+echo "ENTER YOUR SKILL"
+read SKILL
+echo "You have a Nice $SKILL skill"
+read -p 'Username:' USR
+read -sp 'Password' Pass
+echo "WELCOME $USR to Software"
+echo "-----------------------------------------------------------------------------"
