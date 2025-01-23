418cdb24305ae367c5fdcd9ba0fbd046053f5995
keshavp07-git <keshav.prajapati830@gmail.com>
2025-01-23 15:42:26 +0530

    New Method for Monitoring

--- mont.sh
+++ mont.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-ls /var/run/httpd/httpd.pid &> /dev/null
-if [ $? -eq 0 ];then
+echo "----------------------------------------"
+date
+# ls /var/run/httpd/httpd.pid &> /dev/null
+if [ -f /var/run/httpd/httpd.pid ];then # can use also   if [ $? -eq 0 ];then
+echo "----------------------------------------"
 echo "Httpd Process is already running"
+echo "----------------------------------------"
 else
+echo "----------------------------------------"
 echo "Httpd Process is not running"
 echo "Starting Httpd service"
+echo "----------------------------------------"
 systemctl start httpd
 if [ $? -eq 0 ];then
+echo "----------------------------------------"
 echo "Httpd Process Started"
+echo "----------------------------------------"
 else
+echo "----------------------------------------"
 echo "Failed to Start"
+echo "----------------------------------------"
 fi
 fi
